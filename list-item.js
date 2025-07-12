baba8800340f70e88cc80a6d53515f0745457e81
Harsh-12-1 <harshforaiuse@gmail.com>
2025-07-12 17:06:17 +0530

    Update list-item.js

--- list-item.js
+++ list-item.js
@@ -1,12 +1,5 @@
 // Check if user is authenticated
-function checkAuth() {
-    const user = localStorage.getItem('user');
-    if (!user) {
-        window.location.href = 'login.html';
-        return null;
-    }
-    return JSON.parse(user);
-}
+
 
 // Initialize the page
 function initializePage() {
